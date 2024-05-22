59b964e133cf5a8752900ae3588adaf0022e882f
Armelle06 <barban_a@hotmail.com>
2024-05-22 12:13:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# **P12-testeur-logiciel-portfolio**
+
+# **Portfolio de Armelle Barban**
+
+# **Table des matières**
+
+1. Apercu
+2. Technologie Utilisées
+3. Installation
+4. Utilisation
+5. Fonctionnalités
+6. Projets
+7. Optimisations et Corrections
+8. Déploiement
+9. Contact
+
+# **Aperçu**
+
+Ce projet est un portfolio web qui permet de :
+
+-Présenter mon profil de développeur
+-Lister mes compétences techniques
+-Montrer des exemples de projets sur lesquels j'ai travaillé
+
+# **Technologie Utilisées**
+
+-HTML5
+-CSS3
+-JavaScript
+-React (ou autre framework/bibliothèque utilisé)
+-Font Awesome pour les icônes
+-GitHub Pages pour le déploiement
+
+# **Installation**
+
+Pour cloner et exécuter ce projet sur votre machine locale, suivez ces étapes :
+
+1. Clonez le repository:
+
+```
+git clone https://github.com/votre-utilisateur/portfolio.git
+```
+
+2. Accédez au répertoire du projet :
+
+```
+cd portfolio
+
+```
+
+3. Installez les dépendances dans IDE:Visual Studio Code (VSCode):
+
+`https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer`
+
+# **Utilisation**
+
+1. Ouvrez le projet dans Visual Studio Code.
+2. Installez l'extension Live Server si ce n'est pas déjà fait.
+3. Cliquez avec le bouton droit sur le fichier `index.html` et sélectionnez "Open with Live Server".
+   Le site sera accessible à l'adresse `http://127.0.0.1:5500` (ou une autre adresse spécifiée par Live Server).
+
+# **Fonctionnalités**
+
+-**Présentation du profil**: Une section dédiée à mon parcours et à mes compétences. -**Projets** : Présentation détaillée de mes projets avec des liens vers les dépôts GitHub correspondants. -**Responsive Design**: Optimisé pour une visualisation sur différents appareils. -**Accessibilité**: Conforme aux bonnes pratiques en matière de SEO et d'accessibilité.
+
+# **Projets**
+
+# **Optimisations et Corrections**
+
+## **Bugs Résolus**
+
+1. **Icônes Font Awesome non affichées** : Le problème était dû à l'absence du lien vers la feuille de style de Font Awesome dans le fichier HTML. La solution consistait à ajouter le lien suivant dans la balise <head>, avant le lien vers Bootstrap :
+
+```
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer">
+```
+
+2. **Problèmes d'affichage dynamique** : [Description de la correction]
+
+## **Optimisations**
+
+1. **Optimisation des image** : Les images ont été compressées et converties au format WebP pour un chargement plus rapide tout en conservant une haute qualité. Le format WebP permet de réduire la taille des fichiers sans compromettre la qualité visuelle.
+
+2. **Contraste des couleurs** : Le contraste des couleurs a été ajusté pour répondre aux standards d'accessibilité. Cela garantit une meilleure lisibilité pour tous les utilisateurs, y compris ceux ayant des déficiences visuelles.
+
+3. **Structure des titres** : Hiérarchie des titres ajustée pour le SEO et l'accessibilité.
+
+# **Déploiement**
+
+Le site est déployé sur GitHub Pages et est accessible à l'adresse suivante :
+https://votre-utilisateur.github.io/portfolio
+
+# **Contact**
+
+Pour toute question ou collaboration, vous pouvez me contacter via :
+
+--**Email** : armelle.barban@gmail.com
+--**LinkedIn** : `https://www.linkedin.com/in/armelle-barban-511205271/`
---
+++ script/script.js
@@ -0,0 +1,107 @@
+// Fonction pour ajouter la classe "navbarDark" à la barre de navigation lors du défilement
+function handleNavbarScroll() {
+  const header = document.querySelector(".navbar");
+  window.onscroll = function () {
+    const top = window.scrollY;
+    if (top >= 100) {
+      header.classList.add("navbarDark");
+    } else {
+      header.classList.remove("navbarDark");
+    }
+  };
+}
+
+// Fonction pour gérer la rétractation de la barre de navigation sur les petits appareils après un clic
+function handleNavbarCollapse() {
+  const navLinks = document.querySelectorAll(".nav-item");
+  const menuToggle = document.getElementById("navbarSupportedContent");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      new bootstrap.Collapse(menuToggle).toggle();
+    });
+  });
+}
+
+// Fonction pour créer dynamiquement des éléments HTML à partir du fichier JSON
+function createSkillsFromJSON() {
+  const container = document.querySelector("#skills .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
+
+  // Charger le fichier JSON
+  fetch("../data/skills.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Itérer à travers les données JSON data and creation HTML elements
+      data.forEach((item, index) => {
+        //Créer un élément de carte pour chaque compétence
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
+                    <div class="card skillsText">
+                        <div class="card-body">
+                            <img class="border-image" src="./images/${item.image}" alt="${item.title}" loading="lazy"/>
+                            <h3 class="card-title mt-3">${item.title}</h3>
+                            <p class="card-text mt-3">${item.text}</p>
+                        </div>
+                    </div>
+               `;
+        // ajout de loadint mais ca ne fait rien     !!!
+        // Ajouter la carte à la ligne actuelle
+        row.appendChild(card);
+
+        // Si l'index est un multiple de 3 ou s'il s'agit du dernier élément, créer une nouvelle ligne
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
+}
+// Fonction pour créer dynamiquement des éléments HTML à partir du fichier JSON
+function createPortfolioFromJSON() {
+  const container = document.querySelector("#portfolio .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
+
+  // Charger le fichier JSON
+  fetch("data/portfolio.json")
+    .then((response) => response.json())
+    .then((data) => {
+      //Parcourir les données JSON et créer des éléments HTML
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
+                    <div class="card portfolioContent">
+                    <img class="card-img-top" src="images/${item.image}" alt="${item.title}" style="width:100%" loading="lazy">
+                    <div class="card-body">
+                        <h3 class="card-title">${item.title}</h3>
+                        <p class="card-text">${item.text}</p>
+                        <div class="text-center">
+                            <a href="${item.link}" class="btn cli-rose">Lien</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+
+        // Ajouter la carte à la ligne actuelle
+        row.appendChild(card);
+
+        // Si l'index est un multiple de 3 ou si c'est le dernier élément, créer une nouvelle ligne
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
+}
+
+// Appeler les fonctions pour exécuter le code
+handleNavbarScroll();
+handleNavbarCollapse();
+createSkillsFromJSON();
+createPortfolioFromJSON();
